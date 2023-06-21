242416426a6b9f104e6d1e5d3e73a6a841220e30
Ardhy Satrio <rio27dh@gmail.com>
2023-06-22 00:04:47 +0800

    update

--- app/main.py
+++ app/main.py
@@ -17,6 +17,34 @@ img {
 }
 </style>
 """
+
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed with ❤ by <a style='display: block; text-align: center;' href="https://www.heflin.dev/" target="_blank">Heflin Stephen Raj S</a></p>
+</div>
+"""
 # Config
 MAX_SEQ_LEN = 128
 MODELS_PATH = "kadabengaran/IndoBERT-BiLSTM-Useful-App-Review"
@@ -217,3 +245,4 @@ class App:
 if __name__ == "__main__":
     app = App()
     app.run()
+    st.markdown(footer,unsafe_allow_html=True)
